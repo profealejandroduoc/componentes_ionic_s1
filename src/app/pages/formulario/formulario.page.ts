ae186e5a73d52346f0129242b2b5a7d1e99f7492
alejandrossm <alejandrossm@outlook.com>
2024-09-10 17:21:47 -0300

    Form ok

--- src/app/pages/formulario/formulario.page.ts
+++ src/app/pages/formulario/formulario.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario';
 
 @Component({
   selector: 'app-formulario',
@@ -7,12 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioPage implements OnInit {
 
-  constructor() { }
+  usr:Usuario={
+    username:'',
+    password:''
+  }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
   iniciarSesion(){
     console.log("Subimit del formulario");
+    if(this.usr.username=="waco" && this.usr.password=="123"){
+      console.log('autorizado!!!');
+      this.router.navigate(["/home"])
+    }
+    else{
+      console.log("Pa la casa!!!");
+    }
+
   }
+
 }
